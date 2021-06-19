26d960b8331c00e9c841944c04140ed88624853a
amritat123 <83055726+amritat123@users.noreply.github.com>
2021-06-19 13:05:39 +0530

    Add files via upload

---
+++ create_dict_from_string.py
@@ -0,0 +1,14 @@
+# str1='w3resource'
+# dict1={}
+# for i in str1:
+#     dict1[i]=dict1.get(i,0)+1
+# print(dict1)
+#user=int(input("please enter the number"))
+d1={}
+# multi=1
+number=int(input("please enter the number"))
+for i in range(1,number):
+    d1[i]=i*2
+print(d1)
+    
+
